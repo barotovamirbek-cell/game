1c13f9267f93c7921dceaac9ca2d36bafe795e79
barotovamirbek-cell <barotovamirbek@gmail.com>
2025-12-23 11:55:31 +0500

    Update main.py

--- game_bot_FIXED_CD/main.py
+++ game_bot_FIXED_CD/main.py
@@ -1,17 +1,17 @@
 import asyncio
 from aiogram import Bot, Dispatcher
 from config import BOT_TOKEN
-from handlers import start, collect, profile, shop, upgrade
+from handlers import start, menu, donate, clans
+import database  # обязательно
 
 async def main():
     bot = Bot(BOT_TOKEN)
     dp = Dispatcher()
 
     dp.include_router(start.router)
-    dp.include_router(collect.router)
-    dp.include_router(profile.router)
-    dp.include_router(shop.router)
-    dp.include_router(upgrade.router)
+    dp.include_router(menu.router)
+    dp.include_router(donate.router)
+    dp.include_router(clans.router)
 
     await dp.start_polling(bot)
 
