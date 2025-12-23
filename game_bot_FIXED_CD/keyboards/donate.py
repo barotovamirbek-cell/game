1c5babc44004b0495e6f4436000a8567bfbda71a
barotovamirbek-cell <barotovamirbek@gmail.com>
2025-12-23 12:15:28 +0500

    Update donate.py

--- game_bot_FIXED_CD/keyboards/donate.py
+++ game_bot_FIXED_CD/keyboards/donate.py
@@ -3,6 +3,7 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 donate_kb = InlineKeyboardMarkup(
     inline_keyboard=[
         [InlineKeyboardButton("💎 Купить донат", callback_data="donate_buy")],
-        [InlineKeyboardButton("📦 Мой донат", callback_data="donate_my")]
+        [InlineKeyboardButton("📦 Мой донат", callback_data="donate_my")],
+        [InlineKeyboardButton("⬅️ Назад", callback_data="menu_back")]
     ]
 )
