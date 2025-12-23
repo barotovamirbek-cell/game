368c7a3a80287d2704948bba69a59f30ed82848d
barotovamirbek-cell <barotovamirbek@gmail.com>
2025-12-23 12:15:44 +0500

    Update clans.py

--- game_bot_FIXED_CD/keyboards/clans.py
+++ game_bot_FIXED_CD/keyboards/clans.py
@@ -3,6 +3,8 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 clans_menu = InlineKeyboardMarkup(
     inline_keyboard=[
         [InlineKeyboardButton("➕ Создать клан", callback_data="clan_create")],
+        [InlineKeyboardButton("👥 Мой клан", callback_data="clan_my")],
+        [InlineKeyboardButton("🏆 Топ кланов", callback_data="clan_top")],
         [InlineKeyboardButton("⬅️ Назад", callback_data="menu_back")]
     ]
 )
