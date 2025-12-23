04183950e5beb9699bd7d2936a24c2571f7921ab
barotovamirbek-cell <barotovamirbek@gmail.com>
2025-12-23 12:09:52 +0500

    Update database.py

--- game_bot_FIXED_CD/database.py
+++ game_bot_FIXED_CD/database.py
@@ -8,7 +8,11 @@ CREATE TABLE IF NOT EXISTS users (
     user_id INTEGER PRIMARY KEY,
     money INTEGER DEFAULT 0,
     crystals INTEGER DEFAULT 0,
-    clan_id INTEGER DEFAULT NULL
+    last_collect INTEGER DEFAULT 0,
+    last_transfer INTEGER DEFAULT 0,
+    house_level INTEGER DEFAULT 0,
+    car_level INTEGER DEFAULT 0,
+    clan_id INTEGER
 )
 """)
 
@@ -18,7 +22,7 @@ CREATE TABLE IF NOT EXISTS clans (
     name TEXT UNIQUE,
     owner_id INTEGER,
     level INTEGER DEFAULT 1,
-    bank INTEGER DEFAULT 0
+    bonus INTEGER DEFAULT 5
 )
 """)
 
