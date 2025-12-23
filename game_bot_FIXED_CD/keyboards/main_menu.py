971efc96ffdc133de2d55c2c3048392d8241009b
barotovamirbek-cell <barotovamirbek@gmail.com>
2025-12-23 12:16:01 +0500

    Update main_menu.py

--- game_bot_FIXED_CD/keyboards/main_menu.py
+++ game_bot_FIXED_CD/keyboards/main_menu.py
@@ -1,8 +1,17 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-main_menu = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton("💰 Collect", callback_data="noop")],
-    [InlineKeyboardButton("🏪 Shop", callback_data="noop")],
-    [InlineKeyboardButton("🏰 Clans", callback_data="noop")],
-    [InlineKeyboardButton("💎 Donate", callback_data="noop")]
-])
+main_menu = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton("💰 Collect", callback_data="do_collect"),
+            InlineKeyboardButton("👤 Профиль", callback_data="menu_profile")
+        ],
+        [
+            InlineKeyboardButton("🏪 Shop", callback_data="menu_shop"),
+            InlineKeyboardButton("🏰 Clans", callback_data="menu_clans")
+        ],
+        [
+            InlineKeyboardButton("💎 Donate", callback_data="menu_donate")
+        ]
+    ]
+)
